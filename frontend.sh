380890fbdaf7cdd50eff7c4056a72e2e3e9e6ccc
ngowrish <gnavien@gmail.com>
2023-06-28 16:46:46 +0530

    all configuration

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,16 @@
 echo -e "\e[33mInstalling Nginx Server\e[0m"
 yum install nginx -y >/tmp/roboshop.log
+
 echo -e "\e[33mRemoving Old App Content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+
 echo -e "\e[33mDownloading frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+
 echo -e "\e[33mExtract Frontend content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip >/tmp/roboshop.log
+
 #vim /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33mStarting Nginx server\e[0m"
 systemctl enable nginx >/tmp/roboshop.log
