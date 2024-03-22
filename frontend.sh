03de7821bcad5dcb45aac12269ddcddc43d62f97
jayasathyag <jayasathyag@gmail.com>
2024-03-22 19:39:36 +0530

    new update

--- frontend.sh
+++ frontend.sh
@@ -15,6 +15,7 @@ unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 echo -e "\e[32mStarting Nginx server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
+#
 
 
 
