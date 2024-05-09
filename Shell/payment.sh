b5a31264f2123aeea51039511391b84ab271306c
jayasathyag <jayasathyag@gmail.com>
2024-05-09 11:22:05 +0530

    Update the Redis configuration

--- Shell/payment.sh
+++ Shell/payment.sh
@@ -1,26 +1,28 @@
-echo -e "\e[32m\e[0m"
-yum install python36 gcc python3-devel -y
+echo -e "\e[32m Install Pyton Dependencies \e[0m"
+yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
 
-echo -e "\e[32m\e[0m"
-useradd roboshop
+echo -e "\e[32m Adding application  User\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[32m\e[0m"
+echo -e "\e[32m Create Directory\e[0m"
+rm -rf /app
 mkdir /app
 
-echo -e "\e[32m\e[0m"
+echo -e "\e[32m Download application content\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 
-echo -e "\e[32m\e[0m"
+echo -e "\e[32m Extract application content\e[0m"
 cd /app
 unzip /tmp/payment.zip
 
-echo -e "\e[32m\e[0m"
-cd /app
-pip3.6 install -r requirements.txt
+echo -e "\e[32m Install Application Dependencies\e[0m"
+cd /app &>>/tmp/roboshop.log
+pip3.6 install -r requirements.txt  &>>/tmp/roboshop.log
 
-echo -e "\e[32m\e[0m"
-systemctl daemon-reload
+echo -e "\e[32m Setup systemd services\e[0m"
+cp /home/centos/roboshop-shell/Shell/payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
 
-echo -e "\e[32m\e[0m"
-systemctl enable payment
-systemctl restart payment
+echo -e "\e[32mStart Payment  service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment  &>>/tmp/roboshop.log
+systemctl restart payment  &>>/tmp/roboshop.log
--- Shell/rabbitmq.sh
+++ Shell/rabbitmq.sh
@@ -7,13 +7,11 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 echo -e "\e[32m Installing RabbitMq on to the Server\e[0m"
 yum install rabbitmq-server -y  &>>/tmp/roboshop.log
 
-#echo -e "\e[32m Adding user for RabbitMq \e[0m"
-#rabbitmqctl add_user roboshop roboshop123
-
 echo -e "\e[32m start RabbitMq service\e[0m"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl restart rabbitmq-server &>>/tmp/roboshop.log
 
 echo -e "\e[32m Setting of permission to user roboshop\e[0m"
+rabbitmqctl add_user roboshop roboshop123  &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>/tmp/roboshop.log
 
