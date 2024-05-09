dcc36a6beff4f58cef50bbb499330b5e2bbf4ffc
jayasathyag <jayasathyag@gmail.com>
2024-05-09 16:39:34 +0530

    commit

--- Shell/common.sh
+++ Shell/common.sh
@@ -3,26 +3,43 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup() {
+
+   echo -e "${color} Add Application User${noclor}"
+   useradd roboshop &>>${log_file}
+
+   echo -e "${color} Creating application  Directory App${nocolor}"
+   mkdir ${app_path}  &>>${log_file}
+
+   echo -e "${color} Download Application content ${noclor}"
+   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>{log_file}
+
+
+   echo -e "${color} Extract application content${noclor}"
+   cd ${app_path}
+   unzip /tmp/$component.zip &>>log_file
+}
+
+systemd_setup() {
+
+  echo -e "${color} Start $component service ${noclor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable $component &>>${log_file}
+  systemctl restart $component &>>${log_file}
+
+}
 nodejs () {
   echo -e "${color} Configuring Nodejs repos ${noclor}"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log_file
 
+  app_presetup
+
   echo -e "${color} Install Nodejs${noclor}"
   yum install nodejs -y &>>log_file
 
-  echo -e "${color} Add Application User${noclor}"
-  useradd roboshop &>>log_file
 
-  echo -e "${color} Create Application Directory ${noclor}"
-  rm -rf ${app_path}
-  mkdir ${app_path}
+  systemd_setup
 
-  echo -e "${color} Download Application content ${noclor}"
-  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>log_file
-  cd ${app_path}
-
-  echo -e "${color} Extract application content${noclor}"
-  unzip /tmp/$component.zip &>>log_file
 
   echo -e "${color} Install Nodejs dependencies${noclor}"
   cd ${app_path}
@@ -31,19 +48,39 @@ nodejs () {
   echo -e "${color} Setup systemd services${noclor}"
   cp /home/centos/roboshop-shell/Shell/$component.service /etc/systemd/system/$component.service
 
-  echo -e "${color} Start Catalogue service ${noclor}"
-  systemctl daemon-reload &>>log_file
-  systemctl enable $component &>>log_file
-  systemctl restart $component &>>log_file
+
 }
 
 mongodb_schema_setup() {
-  echo -e "\e[32m Copy Mongodb repo file\e[0m"
-  cp /home/centos/roboshop-shell/Shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+  echo -e "${color} Copy Mongodb repo file${nocolor}"
+  cp /home/centos/roboshop-shell/Shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-  echo -e "\e[32m Install MongoDB client\e[0m"
-  yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+  echo -e "${color} Install MongoDB client${nocolor}"
+  yum install mongodb-org-shell -y &>>{log_file}
 
-  echo -e "\e[32m Load Schema\e[0m"
-  mongo --host mongodb-dev.navien.cloud </app/schema/user.js
+  echo -e "${color} Load Schema${nocolor}"
+  mongo --host mongodb-dev.navien.cloud </app/schema/$component.js
+}
+
+mysql_schema_setup() {
+  echo -e "${color} Install MySql${nocolor}"
+  yum install mysql -y  &>>${log_file}
+
+  echo -e "${color} Load Schema${nocolor}"
+  mysql -h mysql-dev.navien.cloud -uroot -pRoboShop@1 < /app/schema/$component.sql  &>>${log_file}
+}
+
+maven() {
+
+  echo -e "${color} Install Maven${nocolor}"
+  yum install maven -y &>>${log_file}
+
+  app_presetup
+  echo -e "${color} Download Maven Dependencies${nocolor}"
+  mvn clean package &>>${log_file}
+  mv target/$component-1.0.jar $component.jar &>>${log_file}
+  mysql_schema_setup
+  systemd_setup
+
+
 }
