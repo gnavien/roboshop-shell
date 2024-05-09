425212ad93f8cd8dc560e97d2b4eaa461d98e009
jayasathyag <jayasathyag@gmail.com>
2024-05-09 08:39:54 +0530

    Update the Redis configuration

---
+++ Shell/user.sh
@@ -0,0 +1,42 @@
+echo -e "\e[32m Configuring Nodejs repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32m Install Nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[32m Add Application User \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[32m Create Application Directory \e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[32m Download Application content \e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[32m Extract application content\e[0m"
+unzip /tmp/user.zip &>>/tmp/roboshop.log
+
+echo -e "\e[32m Install Nodejs dependencies\e[0m"
+cd /app
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[32m Setup systemd services\e[0m"
+cp /home/centos/roboshop-shell/Shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[32m Start user service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl restart user &>>/tmp/roboshop.log
+
+echo -e "\e[32m Copy Mongodb repo file\e[0m"
+cp /home/centos/roboshop-shell/Shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[32m Install MongoDB client\e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[32m Load Schema\e[0m"
+mongo --host mongodb-dev.navien.cloud </app/schema/user.js
+
+
