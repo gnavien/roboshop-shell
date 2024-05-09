c126077f68e483f6c7db9f9de637b7dd4aa371db
jayasathyag <jayasathyag@gmail.com>
2024-05-09 10:09:47 +0530

    Update the Redis configuration

--- Shell/mysql.sh
+++ Shell/mysql.sh
@@ -1,6 +1,9 @@
 echo -e "\e[32mDisable Mysql Installed version\e[0m"
 yum  module disable mysql -y &>>/tmp/roboshop.log
 
+echo -e "\e[32m Copy MySql repo file \e[0m"
+cp  /home/centos/roboshop-shell/Shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
 echo -e "\e[32m Install Mysql Community Server\e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 
@@ -11,5 +14,5 @@ systemctl restart mysqld
 echo -e "\e[32m Setup Mysql Password\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
-echo -e "\e[32m\e[0m"
-mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
+#echo -e "\e[32m\e[0m"
+#mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
