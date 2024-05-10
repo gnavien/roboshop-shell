169e5b76e2fdc06935418c596ebd13fa63fc0985
jayasathyag <jayasathyag@gmail.com>
2024-05-10 18:10:01 +0530

    Final changes done

--- Shell/frontend.sh
+++ Shell/frontend.sh
@@ -3,28 +3,28 @@ component=frontend
 
 echo -e "${color}Installing Nginx Server${nocolor}"
 yum install nginx -y &>>${log_file}
-stat_check
+stat_check $?
 echo -e "${color}Removing Old App Content${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-stat_check
+stat_check $?
 echo -e "${color}Downloading ${component} Content${nocolor}"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-stat_check
+stat_check $?
 
 echo -e "${color}Extract Frontend content${nocolor}"
 cd /usr/share/nginx/html
 unzip /tmp/${component}.zip &>>${log_file}
-stat_check
+stat_check $?
 
 #vim /etc/nginx/default.d/roboshop.conf we need to copy config file
 echo -e "${color} Update Frontend Configuration${nocolor}"
 cp /home/roboshop-shell/Shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
-stat_check
+stat_check $?
 
 echo -e "${color}Starting Nginx server${nocolor}"
 systemctl enable nginx &>>${log_file}
 systemctl restart nginx &>>${log_file}
-stat_check
+stat_check $?
 #
 
 
--- Shell/mongodb.sh
+++ Shell/mongodb.sh
@@ -3,21 +3,21 @@ source common.sh
 
 echo -e "${color}Copy mogo DB Repo file${nocolor}"
 cp /home/roboshop-shell/Shell/mongdb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-stat_check
+stat_check $?
 
 echo -e "${color}Installing MogoDB Server${nocolor}"
 yum install mongodb-org -y &>>${log_file}
-stat_check
+stat_check $?
 
 echo -e "${color}Replacing listen address${nocolor}"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat_check
+stat_check $?
 
 echo -e "${color}Installing MogoDB Server${nocolor}"
 systemctl enable mongod &>>${log_file}
 systemctl restart mongod &>>${log_file}
-stat_check
+stat_check $?
 #vim /etc/mongod.conf (Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf)
 
 
